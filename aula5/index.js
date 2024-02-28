8e5e3478ca5458f448fab5579dbf5cde0acfc921
alicebelchior <alice.belchior23@gmail.com>
2024-02-27 23:41:30 -0300

    aula 5
    continuação da aula sobre manipulação de dados com sequelize

--- aula5/index.js
+++ aula5/index.js
@@ -8,16 +8,40 @@ const sequelize = new Sequelize({
   storage: "empresa.sqlite",
 });
 
-//Antes do sincronismo, criar a tabela (classe) "setor" 
-//a tabela será criada como se fosse uma classe, pois ela tem métodos 
+//Antes do sincronismo, criar a tabela (classe) "setor"
+//a tabela será criada como se fosse uma classe, pois ela tem métodos
 class Setor extends Model {
-    static init(sequelize){
-        super.init({    //esse comando inicia o modulo sequelize, aqui ele começa a criar a tabela dentro do sequelize
-            
-        })   
- 
-    }
+  static init(sequelize) {
+    super.init(
+      {
+        //esse comando inicia o modulo sequelize, aqui ele começa a criar a tabela dentro do sequelize
+        //criação dos campos (atributos) da tabela
+        idsetor: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          allowNull: false,
+          primaryKey: true,
+        },
+        nome: {
+          type: DataTypes.STRING(40),
+          allowNull: false,
+        },
+        ramal: {
+          type: DataTypes.STRING(10),
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING(60),
+        },
+        //Definição do nme da tabela
+      },
+      { sequelize, modelname: "setor", tableName: "setores" }
+    );
+  }
 }
+
+//chamamento do init para solicitar a criação do modelo create table setor
+Setor.init(sequelize);
 
 //Sincronização - ele vai forçar uma atualização do sequelize (BD)
 (async () => {
