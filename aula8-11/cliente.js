9b1a5adc2d214fda509226ca5f1b27e07d9ee75c
Alice Belchior <alice.belchior23@gmail.com>
2024-03-11 14:10:14 -0300

    Criação do model "Cliente" da aplicação

---
+++ aula8-11/cliente.js
@@ -0,0 +1,32 @@
+//criação do model
+const Sequelize = require("sequelize");
+const database = require("./db");
+
+//criação da tabela
+const Cliente = database.define("cliente", {
+    id: {
+        type: Sequelize.INTEGER,
+        AutoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    nascimento: {
+        type: Sequelize.DATE,
+        allowNull: false
+    }, 
+    cidade: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    telefone: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+//exportação para utilização da tabela na aplicação
+module_exports = Cliente;
